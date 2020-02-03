2b91e5cf106df7818ad0f77d0a94e919616af44e
AeroAndZero <ayushthakur667@gmail.com>
2020-02-03 22:45:46 +0530

    Better Algorithm

--- driver.py
+++ driver.py
@@ -1,71 +1,50 @@
 import cv2
 import numpy as np
 import grabscreen
-from directkeys import PressKey, ReleaseKey, W, A, S, D
+from directkeys import PressKey, ReleaseKey, W, A, S, D, SPACE
 import time
-from pynput.keyboard import Key, Controller
 from getkeys import key_check
-keyboard = Controller()
+inputKeys = [W,A,S,D,SPACE]
 
-RLControlPoint = (320,300)
-FWControlPoint = (312,428)
+RLControlPoint = (320,340)
+FWControlPoint = (320,420)
+forwardPress = 0
 
 def forward():
-    '''
-    keyboard.press('w')
-    keyboard.release('a')
-    keyboard.release('s')
-    keyboard.release('d')
-    '''
+    global forwardPress
+    releaseExcept(W)
     PressKey(W)
-    ReleaseKey(A)
-    ReleaseKey(S)
-    ReleaseKey(D)
-    #ReleaseKey(W)
-    print("Forward")
+    forwardPress += 1
 
 def right():
-    '''
-    keyboard.press('d')
-    keyboard.release('w')
-    keyboard.release('s')
-    keyboard.release('d')
-    '''
-    PressKey(D)
-    ReleaseKey(W)
     ReleaseKey(A)
-    ReleaseKey(S)
+    PressKey(D)
 
 def left():
-    '''
-    keyboard.press('a')
-    keyboard.release('w')
-    keyboard.release('s')
-    keyboard.release('d')
-    '''
-    PressKey(A)
     ReleaseKey(D)
-    ReleaseKey(W)
-    ReleaseKey(S)
+    PressKey(A)
 
 def backward():
-    '''
-    keyboard.press('s')
-    keyboard.release('w')
-    keyboard.release('s')
-    keyboard.release('d')
-    '''
+    releaseExcept(S)
     PressKey(S)
-    ReleaseKey(A)
-    ReleaseKey(W)
-    ReleaseKey(D)
 
 def releaseAll():
     ReleaseKey(A)
     ReleaseKey(W)
     ReleaseKey(D)
     ReleaseKey(S)
+    ReleaseKey(SPACE)
 
+def brake():
+    PressKey(SPACE)
+    PressKey(S)
+
+def releaseExcept(key):
+    for inputKey in inputKeys:
+        if key == inputKey:
+            pass
+        else:
+            ReleaseKey(inputKey)
 
 def check(edgeImage,point,direction):
     pointX = point[0]
@@ -91,6 +70,16 @@ def check(edgeImage,point,direction):
         while np.all(edgeImage[pointY,pointX] <= 200) and 0 <= pointY < edgeImage.shape[0]-1 and 0 <= pointX < edgeImage.shape[1]-1:
             pointX -= 1
             distance += 1
+    elif direction == "ne":
+        while np.all(edgeImage[pointY,pointX] <= 200) and 0 <= pointY < edgeImage.shape[0]-1 and 0 <= pointX < edgeImage.shape[1]-1:
+            pointX += 1
+            pointY -= 1
+            distance += 1
+    elif direction == "nw":
+        while np.all(edgeImage[pointY,pointX] <= 200) and 0 <= pointY < edgeImage.shape[0]-1 and 0 <= pointX < edgeImage.shape[1]-1:
+            pointX -= 1
+            pointY -= 1
+            distance += 1
     else:
         print("Invalid Direction")
 
@@ -99,40 +88,68 @@ def check(edgeImage,point,direction):
 time.sleep(7)
 paused = False
 gameLoop = 0
+limit = 10
+brakesOn = 15
+brakeLength = 10
+DiagonalThreshold = 50
+RLThreshold = 60
+
 while True:
     img = grabscreen.grab_screen((0,0,640,480))
-    blur = cv2.GaussianBlur(img,(5,7),1)
-    edge = cv2.Canny(blur,100,120)
+    h,w = img.shape[0],img.shape[1]
+    blur = cv2.GaussianBlur(img,(3,3),1)
     
+    central_part = img[0:h-1,RLControlPoint[0]-limit:RLControlPoint[0]+limit]
+    part_blur = cv2.GaussianBlur(central_part,(15,15),0)
+    
+    blur[0:h-1,RLControlPoint[0]-limit:RLControlPoint[0]+limit] = part_blur
+
+    edge = cv2.Canny(blur,100,120)
+
     if not paused:
-        forward()
-        #if check(edge,FWControlPoint,"n") < 20:
-            #releaseAll()
-        if check(edge,FWControlPoint,"n") < 30 or gameLoop % 15 == 0:
-            releaseAll()
+        if check(edge,FWControlPoint,"n") < 20:
+            brake()
+            print("Slow Down Brakes!")
+
+        '''
+        if forwardPress > 5 :
+            brake()
+            print("Too Much Forward Brakes!")
+            forwardPress = 0
+        '''
+
+        if 0 < check(edge,FWControlPoint,"s") < 30:
             backward()
-        if check(edge,RLControlPoint,"e") < 50:
+            print("Reverse")
+        elif forwardPress > 5:
+            backward()
+            forwardPress = 0
+        else:
+            forward()
+            print("Forward")
+
+        if check(edge,RLControlPoint,"ne") < DiagonalThreshold or check(edge,RLControlPoint,"e") < RLThreshold:
             left()
             print("Went Left")
-        if check(edge,RLControlPoint,"w") < 50:
+        if check(edge,RLControlPoint,"nw") < DiagonalThreshold or check(edge,RLControlPoint,"w") < RLThreshold:
             right()
             print("Went Right")
         
         gameLoop += 1
+    else:
+        print("AI Paused")
 
     cv2.imshow("Edges",edge)
 
     keys = key_check()
-    if 'T' in keys:
+    if 'P' in keys:
             if paused:
                 paused = False
-                time.sleep(1)
             else:
                 paused = True
                 ReleaseKey(A)
                 ReleaseKey(W)
                 ReleaseKey(D)
-                time.sleep(1)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
