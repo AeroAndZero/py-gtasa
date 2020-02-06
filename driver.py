b1e557063fb179b43f2352dc3923e5661d9d88cf
AeroAndZero <ayushthakur667@gmail.com>
2020-02-06 20:54:40 +0530

    COMMENTS ARE IMPORTANT
    ffs

--- driver.py
+++ driver.py
@@ -6,13 +6,12 @@ import time
 from getkeys import key_check
 inputKeys = [W,A,S,D,SPACE]
 
-#Sorry which branch is this ?
-
 #RLControlPoint = (320,340)
 RLControlPoint = (320,360)
 FWControlPoint = (320,340)
 forwardPress = 0
 
+#Basic movement operations
 def forward():
     global forwardPress
     releaseExcept(W)
@@ -31,16 +30,16 @@ def backward():
     releaseExcept(S)
     PressKey(S)
 
+def brake():
+    PressKey(SPACE)
+    PressKey(S)
+
 def releaseAll():
     ReleaseKey(A)
     ReleaseKey(W)
     ReleaseKey(D)
     ReleaseKey(S)
     ReleaseKey(SPACE)
-
-def brake():
-    PressKey(SPACE)
-    PressKey(S)
 
 def releaseExcept(key):
     for inputKey in inputKeys:
@@ -49,6 +48,7 @@ def releaseExcept(key):
         else:
             ReleaseKey(inputKey)
 
+#returns distance between control point and first white pixel is detects
 def check(edgeImage,point,direction):
     pointX = point[0]
     pointY = point[1]
@@ -99,15 +99,13 @@ def check(edgeImage,point,direction):
 
     return distance,pointX,pointX
 
-time.sleep(7)
-paused = True
-gameLoop = 0
-limit = 10
-brakesOn = 15
-brakeLength = 10
-DiagonalThreshold = 45
-superDiagonalThreshold = 20
-RLThreshold = 50
+#Global variables
+time.sleep(7)   #For giving some time before script start
+paused = True   #For pausing the game
+gameLoop = 0    #keeps track of total processed frames
+limit = 10      #for FWcontrolPoint part selection
+DiagonalThreshold = 45  #threshold for north-east and north-west direction
+RLThreshold = 50    #threshold for east and west direction
 
 #Line Detection Parameters
 rho = 1  # distance resolution in pixels of the Hough grid
@@ -117,67 +115,86 @@ min_line_length = 60  # minimum number of pixels making up a line
 max_line_gap = 10  # maximum gap in pixels between connectable line segments
 
 while True:
+    #Resetting control points
     pointX,pointY = RLControlPoint
 
+    #Getting gameplay footage
     img = grabscreen.grab_screen((0,0,640,480))
     line_image = np.zeros_like(img)
     h,w = img.shape[0],img.shape[1]
     blur = cv2.GaussianBlur(img,(3,3),1)
     
+    #For FWcontrolPoint
     central_part = img[0:h-1,RLControlPoint[0]-limit:RLControlPoint[0]+limit]
     part_blur = cv2.GaussianBlur(central_part,(15,15),0)
-    
     blur[0:h-1,RLControlPoint[0]-limit:RLControlPoint[0]+limit] = part_blur
 
+    #Acutal edge detection
     edge = cv2.Canny(blur,100,120)
 
+    #Finding Lines
     lines = cv2.HoughLinesP(edge, rho, theta, threshold, np.array([]),
                     min_line_length, max_line_gap)
 
+    #drawing lines on a different image
     if lines is not None:
         for line in lines:
             for x1,y1,x2,y2 in line:
                 cv2.line(line_image,(x1,y1),(x2,y2),(255,255,255),5)
     
+    #Just a handy variable
     processImage = line_image
 
+    #Condition checking
     if not paused:
+        #For slowing down
         if check(processImage,FWControlPoint,"n")[0] < 20:
             brake()
             print("Slow Down Brakes!")
 
+        #If car crashes, take reverse
         if 0 < check(processImage,FWControlPoint,"s")[0] < 30:
             backward()
             print("Reverse")
+        #For Slowing down
         elif forwardPress > 5:
             backward()
             forwardPress = 0
+        #Forward obviously
         else:
             forward()
             print("Forward")
 
-        if 10 < check(processImage,RLControlPoint,"ne")[0] < DiagonalThreshold or 10 < check(processImage,RLControlPoint,"e")[0] < RLThreshold: #or check(processImage,RLControlPoint,"nne") < superDiagonalThreshold:
+        #Condition for turning left
+        if 10 < check(processImage,RLControlPoint,"ne")[0] < DiagonalThreshold or 10 < check(processImage,RLControlPoint,"e")[0] < RLThreshold:
             pointX,pointY = check(processImage,RLControlPoint,"ne")[1],check(processImage,RLControlPoint,"ne")[2]
             left()
             print("Left")
             
+            #for output display
             cv2.line(processImage,(pointX,pointY),RLControlPoint,(0,0,255),2)
-        if 10 < check(processImage,RLControlPoint,"nw")[0] < DiagonalThreshold or 10 < check(processImage,RLControlPoint,"w")[0] < RLThreshold: #or check(processImage,RLControlPoint,"nnw") < superDiagonalThreshold:
+        
+        #Condition for turning right
+        if 10 < check(processImage,RLControlPoint,"nw")[0] < DiagonalThreshold or 10 < check(processImage,RLControlPoint,"w")[0] < RLThreshold:
             pointX,pointY = check(processImage,RLControlPoint,"nw")[1],check(processImage,RLControlPoint,"nw")[2]
             right()
             print("Right")
             
+            #for output display
             cv2.line(processImage,(pointX,pointY),RLControlPoint,(0,0,255),2)
         
+        #Keep track of gameloop
         gameLoop += 1
     else:
+        #For proof that I am not driving
         print("AI Paused")
 
+    #Showing Output
     cv2.circle(processImage,RLControlPoint,2,(0,0,255),3)
     cv2.circle(processImage,FWControlPoint,2,(0,0,255),3)
-
     cv2.imshow("Lines",processImage)
 
+    #Pausing is important
     keys = key_check()
     if 'P' in keys:
             if paused:
@@ -190,7 +207,9 @@ while True:
                 ReleaseKey(D)
                 time.sleep(1)
 
+    #Quiting opencv
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+#Destroooyyyyyyy
 cv2.destroyAllWindows()
