cb53fb90b4d6759df128677025c0dd89df5aaf8f
AeroAndZero <ayushthakur667@gmail.com>
2020-02-11 19:37:54 +0530

    init Commit

--- driver.py
+++ driver.py
@@ -10,6 +10,9 @@ inputKeys = [W,A,S,D,SPACE]
 RLControlPoint = (320,360)
 FWControlPoint = (320,340)
 forwardPress = 0
+#Map Pixels :
+#   Top-left = 32,360
+#   bottom-right = 142,459
 
 #Basic movement operations
 def forward():
@@ -100,7 +103,7 @@ def check(edgeImage,point,direction):
     return distance,pointX,pointX
 
 #Global variables
-time.sleep(7)   #For giving some time before script start
+time.sleep(3)   #For giving some time before script start
 paused = True   #For pausing the game
 gameLoop = 0    #keeps track of total processed frames
 limit = 10      #for FWcontrolPoint part selection
@@ -110,9 +113,9 @@ RLThreshold = 50    #threshold for east and west direction
 #Line Detection Parameters
 rho = 1  # distance resolution in pixels of the Hough grid
 theta = np.pi / 180  # angular resolution in radians of the Hough grid
-threshold = 50  # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 60  # minimum number of pixels making up a line
-max_line_gap = 10  # maximum gap in pixels between connectable line segments
+threshold = 30  # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 20  # minimum number of pixels making up a line
+max_line_gap = 2  # maximum gap in pixels between connectable line segments
 
 while True:
     #Resetting control points
@@ -120,19 +123,12 @@ while True:
 
     #Getting gameplay footage
     img = grabscreen.grab_screen((0,0,640,480))
-    line_image = np.zeros_like(img)
-    h,w = img.shape[0],img.shape[1]
-    blur = cv2.GaussianBlur(img,(3,3),1)
-    
-    #For FWcontrolPoint
-    central_part = img[0:h-1,RLControlPoint[0]-limit:RLControlPoint[0]+limit]
-    part_blur = cv2.GaussianBlur(central_part,(15,15),0)
-    blur[0:h-1,RLControlPoint[0]-limit:RLControlPoint[0]+limit] = part_blur
 
-    #Acutal edge detection
-    edge = cv2.Canny(blur,100,120)
+    #Extracting Map
+    gameMap_org = img[360:459,32:142]
+    edge = cv2.Canny(gameMap_org,70,20)
 
-    #Finding Lines
+    #line detection
     lines = cv2.HoughLinesP(edge, rho, theta, threshold, np.array([]),
                     min_line_length, max_line_gap)
 
@@ -140,59 +136,18 @@ while True:
     if lines is not None:
         for line in lines:
             for x1,y1,x2,y2 in line:
-                cv2.line(line_image,(x1,y1),(x2,y2),(255,255,255),5)
-    
-    #Just a handy variable
-    processImage = line_image
+                cv2.line(gameMap_org,(x1,y1),(x2,y2),(255,255,255),5)
+
 
     #Condition checking
     if not paused:
-        #For slowing down
-        if check(processImage,FWControlPoint,"n")[0] < 20:
-            brake()
-            print("Slow Down Brakes!")
-
-        #If car crashes, take reverse
-        if 0 < check(processImage,FWControlPoint,"s")[0] < 30:
-            backward()
-            print("Reverse")
-        #For Slowing down
-        elif forwardPress > 5:
-            backward()
-            forwardPress = 0
-        #Forward obviously
-        else:
-            forward()
-            print("Forward")
-
-        #Condition for turning left
-        if 10 < check(processImage,RLControlPoint,"ne")[0] < DiagonalThreshold or 10 < check(processImage,RLControlPoint,"e")[0] < RLThreshold:
-            pointX,pointY = check(processImage,RLControlPoint,"ne")[1],check(processImage,RLControlPoint,"ne")[2]
-            left()
-            print("Left")
-            
-            #for output display
-            cv2.line(processImage,(pointX,pointY),RLControlPoint,(0,0,255),2)
-        
-        #Condition for turning right
-        if 10 < check(processImage,RLControlPoint,"nw")[0] < DiagonalThreshold or 10 < check(processImage,RLControlPoint,"w")[0] < RLThreshold:
-            pointX,pointY = check(processImage,RLControlPoint,"nw")[1],check(processImage,RLControlPoint,"nw")[2]
-            right()
-            print("Right")
-            
-            #for output display
-            cv2.line(processImage,(pointX,pointY),RLControlPoint,(0,0,255),2)
-        
+        #Not Paused
+        print("Running..")
         #Keep track of gameloop
         gameLoop += 1
     else:
         #For proof that I am not driving
         print("AI Paused")
-
-    #Showing Output
-    cv2.circle(processImage,RLControlPoint,2,(0,0,255),3)
-    cv2.circle(processImage,FWControlPoint,2,(0,0,255),3)
-    cv2.imshow("Lines",processImage)
 
     #Pausing is important
     keys = key_check()
@@ -206,6 +161,9 @@ while True:
                 ReleaseKey(W)
                 ReleaseKey(D)
                 time.sleep(1)
+
+    gameMap = cv2.resize(gameMap_org,(500,500))
+    cv2.imshow("Map",gameMap)
 
     #Quiting opencv
     if cv2.waitKey(1) & 0xFF == ord('q'):
