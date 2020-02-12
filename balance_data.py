07f9df44c246b4611bc88202d5fffa02173d7b37
Ayush Thakur <ayushthakur667@gmail.com>
2020-02-12 11:21:22 +0530

    Removing Only Forwards
    This will remove the data where i only pressed the "W" key

--- balance_data.py
+++ balance_data.py
@@ -9,17 +9,8 @@ df = pd.DataFrame(train_data)
 print(df.head())
 print(Counter(df[1].apply(str)))
 
-lefts = []
-rights = []
-forwards = []
-backwards = []
-spaces = []
-
-
-#dists = np.array([i[0] for i in train_data])
-#keys = np.array([j[1] for j in train_data])
-
-
+onlyForwards = []
+everythingElse = []
 
 shuffle(train_data)
 
@@ -28,27 +19,11 @@ for data in train_data:
     keys = data[1]
     
     if keys == [1,0,0,0,0]:
-        forwards.append([dists,keys])
-    elif keys == [0,1,0,0,0]:
-        backwards.append([dists,keys])
-    elif keys == [0,0,1,0,0]:
-        lefts.append([dists,keys])
-    elif keys == [0,0,0,1,0]:
-        rights.append([dists,keys])
-    elif keys == [0,0,0,0,1]:
-        spaces.append([dists,keys])
+        onlyForwards.append([dists,keys])
     else:
-        print('no matches')
+        everythingElse.append([dists,keys])
 
-#print(keys)
-forwards = forwards[:len(lefts)][:len(rights)][:len(backwards)][:len(spaces)]
-lefts = lefts[:len(forwards)]
-rights = rights[:len(forwards)]
-backwards = backwards[:len(forwards)]
-spaces = spaces[:len(forwards)]
-
-
-final_data = forwards + lefts + rights + backwards + spaces
+final_data = everythingElse
 
 shuffle(final_data)
 
